3b0d8e9ad66d420d0162f713abcf29de6bff9fd6
Documenter.jl <documenter@juliadocs.github.io>
2023-04-11 16:02:34 +0000

    build based on 1675f37

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AnalyticPulses","category":"page"},{"location":"#AnalyticPulses","page":"Home","title":"AnalyticPulses","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AnalyticPulses.","category":"page"},{"location":"","page":"Home","title":"Home","text":"This package contains analytical solutions to very simple quantum control problems.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AnalyticPulses]","category":"page"}]
+}
